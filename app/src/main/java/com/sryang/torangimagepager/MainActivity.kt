88ecc9a1fef3feca3a8adf608f90a65feb6015fe
sryang <sarang628@gmail.com>
2024-05-24 18:16:35 +0800

    update

--- app/src/main/java/com/sryang/torangimagepager/MainActivity.kt
+++ app/src/main/java/com/sryang/torangimagepager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sryang.torangimagepager
 
+import ZoomableTorangAsyncImage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,7 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.sryang.imagepager.ImagePager
+import com.sryang.library.ImagePagerWithContents
 import com.sryang.torangimagepager.ui.theme.TorangImagePagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TorangImagePagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ImagePagerWithContentsTest()
                 }
             }
         }
@@ -42,4 +43,49 @@ fun GreetingPreview() {
     TorangImagePagerTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun ImagePagerWithContentsTest() {
+    ImagePagerWithContents(
+        list = listOf(
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/09_19_29_616.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/09_19_29_653.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/09_30_07_284.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/09_30_07_325.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/09_59_45_505.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/09_59_45_535.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/10_12_56_409.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/10_12_56_453.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/10_14_24_779.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/10_14_41_930.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/10_45_42_899.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/10_45_42_943.jpg",
+            "http://sarang628.iptime.org:89/review_images/0/0/2023-09-14/10_45_43_007.jpg"
+        ),
+        position = 0,
+        date = "MAY 10 AT 6:40 PM",
+        likeCount = "1.7K",
+        name = "Torang",
+        contents = "contents",
+        commentCount = "762 comments",
+        onName = {},
+        onDate = {},
+        onContents = {},
+        onLike = {},
+        onComment = {},
+        imagePager = { list, position, onPage, backgroundColor ->
+            ImagePager(
+                list = list,
+                position = position ?: 0,
+                onPage = onPage,
+                backgroundColor = backgroundColor ?: Color.Black,
+                image = { url ->
+                    ZoomableTorangAsyncImage(
+                        model = url,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                })
+        }
+    )
 }
---
+++ library/src/main/java/com/sryang/library/ImagePagerWithContents.kt
@@ -0,0 +1,180 @@
+package com.sryang.library
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+@Composable
+fun ImagePagerWithContents(
+    list: List<String>,
+    position: Int = 0,
+    date: String, // MAY 10 AT 6:40 PM,
+    likeCount: String, //"1.7K"
+    name: String, // Torang
+    contents: String, // contents,
+    commentCount: String,//"762 comments"
+    onName: (() -> Unit)? = null,
+    onDate: (() -> Unit)? = null,
+    onContents: (() -> Unit)? = null,
+    onLike: (() -> Unit)? = null,
+    onComment: (() -> Unit)? = null,
+    imagePager: @Composable (
+        list: List<String>,
+        position: Int?,
+        onPage: ((Int) -> Unit)?,
+        backgroundColor: Color?,
+    ) -> Unit,
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        constraintSet = imagePagerConstraintSet()
+    ) {
+        Scaffold(
+            Modifier
+                .fillMaxSize(),
+            containerColor = Color.Transparent
+
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+                    .background(Color.Transparent),
+            ) {
+
+
+                imagePager.invoke(list, position, null, null)
+
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(start = 12.dp, end = 12.dp)
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .layoutId("name")
+                            .clickable {
+                                onName?.invoke()
+                            },
+                        text = name, color = Color.White, fontWeight = FontWeight.Bold
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .layoutId("contents")
+                            .clickable {
+                                onContents?.invoke()
+                            },
+                        text = contents, color = Color.White, fontSize = 12.sp
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .layoutId("date")
+                            .clickable {
+                                onDate?.invoke()
+                            },
+                        text = date, color = Color.LightGray, fontSize = 12.sp
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                modifier = Modifier
+                                    .size(15.dp)
+                                    .clickable {
+                                        onLike?.invoke()
+                                    },
+                                painter = painterResource(id = R.drawable.heart_filled),
+                                contentDescription = "",
+                                tint = Color.Red
+                            )
+                            Spacer(modifier = Modifier.width(3.dp))
+                            Text(text = likeCount, color = Color.White, fontSize = 12.sp)
+                        }
+                        Text(
+                            text = commentCount,
+                            color = Color.White,
+                            modifier = Modifier
+                                .align(Alignment.CenterEnd)
+                                .clickable {
+                                    onComment?.invoke()
+                                }, fontSize = 12.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun imagePagerConstraintSet(): ConstraintSet {
+    return ConstraintSet {
+        val imagePager = createRefFor("imagePager")
+        val name = createRefFor("name")
+        val date = createRefFor("date")
+        val contents = createRefFor("contents")
+
+        constrain(imagePager) {
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+        }
+
+        constrain(contents) {
+            top.linkTo(name.bottom)
+        }
+
+        constrain(date) {
+            top.linkTo(contents.bottom)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewImagePagerWithContents() {
+    ImagePagerWithContents(
+        list = listOf(),
+        position = 0,
+        date = "MAY 10 AT 6:40 PM",
+        likeCount = "1.7K",
+        name = "Torang",
+        contents = "contents",
+        commentCount = "762 comments",
+        onName = {},
+        onDate = {},
+        onContents = {},
+        onLike = {},
+        onComment = {},
+        imagePager = { _, _, _, _ ->
+        }
+    )
+}
