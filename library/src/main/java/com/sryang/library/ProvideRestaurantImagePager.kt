7ce7dea6ca52742e67fb3d8d207473b716ffa25a
sryang <sarang628@gmail.com>
2024-05-25 13:10:31 +0800

    update

--- library/src/main/java/com/sryang/library/ProvideRestaurantImagePager.kt
+++ library/src/main/java/com/sryang/library/ProvideRestaurantImagePager.kt
@@ -1,14 +1,9 @@
 package com.sryang.library
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sryang.library.ui.component.ImagePagerWithContents
@@ -42,6 +37,7 @@ fun provideRestaurantImagePager(
             commentCount = uiState.commentCount,
             imagePager = imagePager,
             image = image,
+            position = uiState.position,
             onPage = { viewModel.onPage(it) }
         )
     }
